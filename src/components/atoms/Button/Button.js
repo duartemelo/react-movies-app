5e2db33de3fa742c9bc12f2811a75dcfc7f36d75
duartemelo <duarteribeirodemelo@hotmail.com>
2023-03-14 01:26:42 +0000

    Button animate

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-  const styleObj = {
+
+  const preStyleObj = {
     backgroundColor: props.backgroundColor,
     color: props.color,
     width: props.width,
     height: props.height,
     fontWeight: props.fontWeight,
-    fontSize: props.fontSize
+    fontSize: props.fontSize,
+  }
+
+  const [styleObj, setStyleObj] = useState(preStyleObj);
+
+  const handleMouseEnter = () => {
+    setStyleObj((prevState) => {
+      return {...prevState, backgroundColor: props.color, color: props.backgroundColor}
+    })
   };
-  return(
-    <button className={`${props.className} ${classes["default-button"]}`} style={styleObj}>{props.children}</button>
-  )
-}
+
+  const handleMouseLeave = () => {
+    setStyleObj(preStyleObj);
+  };
+
+  return (
+    <button
+      className={`${props.className} ${classes["default-button"]}`}
+      style={styleObj}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
