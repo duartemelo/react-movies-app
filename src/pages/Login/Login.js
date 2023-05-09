76d184faa60cb90cf08ec55c92b2878988ae4507
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-05-09 14:56:59 +0100

    Login input validation in progress

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,23 +5,38 @@ import Input from "../../components/atoms/Input/Input";
 import Button from "../../components/atoms/Button/Button";
 import classes from "./Login.module.css";
 import useFirebase from "../../hooks/use-firebase";
+import useInput from "../../hooks/use-input";
 
 const Login = () => {
-  // TODO: input validation!
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // TODO: input validation! (in progress)
 
   // TODO: use isLoading for spinner inside LoginButton
   const { isLoading, error, login } = useFirebase();
-
   const navigate = useNavigate();
+
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChanged,
+    inputBlurHandler: emailBlur,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: password,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChanged,
+    inputBlurHandler: passwordBlur,
+  } = useInput((value) => value.trim() !== "");
 
   const handleLogin = (e) => {
     e.preventDefault();
-    login(email, password, (userCredential) => {
-      navigate("/popular/1");
-    });
+    if (emailIsValid && passwordIsValid) {
+      login(email, password, () => {
+        navigate("/popular/1");
+      });
+    }
   };
 
   const errorContent = (errorCode) =>
@@ -39,19 +54,22 @@ const Login = () => {
           placeholder="E-mail"
           className="mt-4"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={emailChanged}
+          onBlur={emailBlur}
           type="email"
           required
         />
+        {emailHasError && <p>Email can't be empty.</p>}
         <Input
           placeholder="Password"
           className="mt-2"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={passwordChanged}
+          onBlur={passwordBlur}
           type="password"
           required
         />
-        {/* TODO: styling! */}
+        {passwordHasError && <p>Password can't be empty.</p>}
         {error && <p>{errorContent(error.errorCode)}</p>}
         <div className={classes["action-container"]}>
           <Button className="secondary" type="submit">
