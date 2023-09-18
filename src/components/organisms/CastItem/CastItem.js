0265e99a47339be2bc30e270b3c2d487ac2233a7
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-09-18 01:08:13 +0100

    CastItem image click goes to person page

--- src/components/organisms/CastItem/CastItem.js
+++ src/components/organisms/CastItem/CastItem.js
@@ -1,28 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
+import classes from "./CastItem.module.css";
+
 import Tooltip from "../../atoms/Tooltip/Tooltip";
 
 import Image from "../../molecules/Image/Image";
 
-
-
 const CastItem = (props) => {
   const navigate = useNavigate();
 
+  const castItemClickHandler = () => {
+    navigate(`/person/${props.personId}`);
+  };
+
   return (
-    <Tooltip
-      toolTipId={props.personId}
-      toolTipText={props.personName}
-    >
-      <Image
-        key={props.personName}
-        alt={props.personName}
-        width="60px"
-        height="60px"
-        borderRadius="9999px"
-        imageSrc={"https://image.tmdb.org/t/p/w342" + props.imagePath}
-      />
+    <Tooltip toolTipId={props.personId} toolTipText={props.personName}>
+      <div className={classes["image-wrapper"]} onClick={castItemClickHandler}>
+        <Image
+          key={props.personName}
+          alt={props.personName}
+          width="60px"
+          height="60px"
+          borderRadius="9999px"
+          imageSrc={"https://image.tmdb.org/t/p/w342" + props.imagePath}
+        />
+      </div>
     </Tooltip>
   );
 };
